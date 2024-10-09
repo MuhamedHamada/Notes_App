8fca6545185782c1f1f8d3d389ff967a24030450
MuhamedHamada <mohamedsscc11@gmail.com>
2024-10-09 17:28:25 +0300

    Trigger and note cubit

--- lib/widgets/AddNoteForm.dart
+++ lib/widgets/AddNoteForm.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notesapp/cubits/AddNoteCubit.dart';
+import 'package:notesapp/models/note_model.dart';
 
 import 'CustomButton.dart';
 import 'CustomTextField.dart';
@@ -49,6 +52,8 @@ class _AddNoteFormState extends State<AddNoteForm> {
             onTap: (){
               if(formKey.currentState!.validate()){
                 formKey.currentState!.save();
+                var noteModel=NoteModel(title: title!, subTitle: subTitle!, date: DateTime.now().toString(), color: Colors.blue.value);
+                BlocProvider.of<AddNoteCubit>(context).addNote(noteModel);
               }
               else{
                 autovalidateMode=AutovalidateMode.always;
--- lib/widgets/addNote.dart
+++ lib/widgets/addNote.dart
@@ -14,22 +14,20 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SingleChildScrollView(
-        child: BlocConsumer<AddNoteCubit, AddNoteState>(
-          listener: (context, state) {
-            if(state is AddNoteFailure){
-              print("failed");
-            }
-if(state is AddNoteSuccess){
-  Navigator.pop(context);
-}
-          },
-          builder: (context, state) {
-            return ModalProgressHUD(
-              inAsyncCall: state is AddNoteLoading? true :false,
-                child: AddNoteForm());
-          },
-        ),
+      child: BlocConsumer<AddNoteCubit, AddNoteState>(
+        listener: (context, state) {
+          if(state is AddNoteFailure){
+            print("failed");
+          }
+      if(state is AddNoteSuccess){
+        Navigator.pop(context);
+      }
+        },
+        builder: (context, state) {
+          return ModalProgressHUD(
+            inAsyncCall: state is AddNoteLoading? true :false,
+              child: SingleChildScrollView(child: AddNoteForm()));
+        },
       ),
     );
   }
